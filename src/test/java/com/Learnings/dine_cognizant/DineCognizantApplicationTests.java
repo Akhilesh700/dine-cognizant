4abe97abf5f42969a2994bd92c3bb306e41afd70
Salve <2440574@cognizant.com>
2025-09-09 09:16:50 +0530

    Initialization: this is the initial comment.

---
+++ src/test/java/com/Learnings/dine_cognizant/DineCognizantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Learnings.dine_cognizant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineCognizantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
